5c36e6042b940c7238449e361d434fb3f7abcf9d
jthonen <thonen.jacob@gmail.com>
2018-09-25 12:15:53 -0700

    initialize firebase added

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,29 @@
 $(document).ready(function(){
+   
+    // Initialize Firebase
+
+    // Firebase Address -
+    // https://console.firebase.google.com/u/0/project/trainschedule-4dd32/overview
+
+    var config = {
+        apiKey: "AIzaSyDfHIXAWt9Wjew2sOyz4Tf03kludlj20_U",
+        authDomain: "trainschedule-4dd32.firebaseapp.com",
+        databaseURL: "https://trainschedule-4dd32.firebaseio.com",
+        projectId: "trainschedule-4dd32",
+        storageBucket: "",
+        messagingSenderId: "390190264947"
+    };
+    firebase.initializeApp(config);
+    
+    // Variables for holding train information
 
     var trainname = $("#train-name-input").val();
     var destination = $("#destiantion-name-input").val();
     var firsttraintime = $("#first-train-time-input").val();
     var frequecy = $("#frequency-input").val();
 
+    // On click Handler for the submit button
+    
     $(".submit-button").on('click', function(event){
         event.preventdefault();
 
@@ -17,6 +36,7 @@ $(document).ready(function(){
         }
     })
 
+    // Add Train function with variables to grab the data and put into a table format. then append to the tablebody div.
 
     function addtrain(){
 
