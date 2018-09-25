f1cfbe163b0e926e0cf5f495ecbcc9a0db62ef56
jthonen <thonen.jacob@gmail.com>
2018-09-25 11:27:19 -0700

    update

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,38 @@
 $(document).ready(function(){
 
-    
+    var trainname = $("#train-name-input").val();
+    var destination = $("#destiantion-name-input").val();
+    var firsttraintime = $("#first-train-time-input").val();
+    var frequecy = $("#frequency-input").val();
+
+    $(".submit-button").on('click', function(event){
+        event.preventdefault();
+
+        if (trainname === '' & destination === '' & firsttraintime === '' & frequecy === '') {
+            console.log("Please Don't Do That.");
+            return false;
+        }
+        else {
+            addtrain();
+        }
+    })
+
+
+    function addtrain(){
+
+        var trainname = $("#train-name-input").val();
+        var destination = $("#destiantion-name-input").val();
+        var firsttraintime = $("#first-train-time-input").val();
+        var frequecy = $("#frequency-input").val();
+        var nextarrival = '';
+        var minutesaway = '';
+        var tableconsole = trainname | destination | firsttraintime | frequecy | nextarrival | minutesaway;
+        
+        console.log(tableconsole);
+
+        var table = $("<tr><th scope='row'>" + trainname + "</th><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextarrival + "</td><td>" + minutesaway + "</td></tr>");
+
+        $(".train-table").append(table);
+
+    }
 })
